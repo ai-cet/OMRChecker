f0112c472ff1d1c70f44764bc953abd635d657fc
Wai Kay <waikay@comp.nus.edu.sg>
2019-11-07 14:43:39 +0800

    Added additonal options

--- utils.py
+++ utils.py
@@ -24,37 +24,37 @@ from random import randint
 from time import localtime,strftime,time
 # from skimage.filters import threshold_adaptive
 
-
-print('Checking Directories...')
-for _dir in [saveMarkedDir]:
-    if(not os.path.exists(_dir)):
-        print('Created : '+ _dir)
-        os.makedirs(_dir)
-        for sl in ['HE','JE']:#,'HH','JH']:
-            os.mkdir(_dir+sl)
-            os.mkdir(_dir+sl+'/stack')
-            os.mkdir(_dir+sl+'/_MULTI_')
-            os.mkdir(_dir+sl+'/_MULTI_'+'/stack')
-            # os.mkdir(_dir+sl+'/_BADSCAN_')
-            # os.mkdir(_dir+sl+'/_BADSCAN_'+'/stack')
-    else:
-        print('Present : '+_dir)
-
-for _dir in [manualDir,resultDir]:
-    if(not os.path.exists(_dir)):
+def check_dirs(paths):
+    print('Checking Directories...')
+    for _dir in [paths.saveMarkedDir]:
+        if(not os.path.exists(_dir)):
             print('Created : '+ _dir)
             os.makedirs(_dir)
-    else:
-        print('Present : '+_dir)
+            for sl in ['HE','JE']:#,'HH','JH']:
+                os.mkdir(_dir+sl)
+                os.mkdir(_dir+sl+'/stack')
+                os.mkdir(_dir+sl+'/_MULTI_')
+                os.mkdir(_dir+sl+'/_MULTI_'+'/stack')
+                # os.mkdir(_dir+sl+'/_BADSCAN_')
+                # os.mkdir(_dir+sl+'/_BADSCAN_'+'/stack')
+        else:
+            print('Present : '+_dir)
 
-for _dir in [multiMarkedDir,errorsDir,badRollsDir]:
-    if(not os.path.exists(_dir)):
-        print('Created : '+ _dir)
-        os.makedirs(_dir)
-        for sl in ['HE','JE']:#,'HH','JH']:
-            os.mkdir(_dir+sl)
-    else:
-        print('Present : '+_dir)
+    for _dir in [paths.manualDir,paths.resultDir]:
+        if(not os.path.exists(_dir)):
+                print('Created : '+ _dir)
+                os.makedirs(_dir)
+        else:
+            print('Present : '+_dir)
+
+    for _dir in [paths.multiMarkedDir,paths.errorsDir,paths.badRollsDir]:
+        if(not os.path.exists(_dir)):
+            print('Created : '+ _dir)
+            os.makedirs(_dir)
+            for sl in ['HE','JE']:#,'HH','JH']:
+                os.mkdir(_dir+sl)
+        else:
+            print('Present : '+_dir)
 
 
 # In[64]:
@@ -153,8 +153,8 @@ def drawTemplateLayout(img, template, shifted=True, draw_qvals=False, border=-1)
 
 def getPlotImg():
     plt.savefig('tmp.png')
-    # img = cv2.imread('tmp.png',cv2.IMREAD_COLOR)
-    img = cv2.imread('tmp.png',cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('tmp.png',cv2.IMREAD_COLOR)
+    # img = cv2.imread('tmp.png',cv2.IMREAD_GRAYSCALE)
     os.remove("tmp.png")
     # plt.cla() 
     # plt.clf()
@@ -414,6 +414,55 @@ clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(8,8))
 thresholdCircles=[]
 badThresholds=[]
 veryBadPoints=[]
+
+ 
+''' 
+Code obtained from https://www.learnopencv.com/image-alignment-feature-based-using-opencv-c-python/
+'''
+def alignImages(im1, im2):
+    MAX_FEATURES = 500
+    GOOD_MATCH_PERCENT = 0.15
+
+    # Convert images to grayscale
+    im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+    
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+    
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+    
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+    
+    # Draw top matches
+    imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    cv2.imwrite("matches.jpg", imMatches)
+    
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+    
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+    
+    # Find homography
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+    
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv2.warpPerspective(im1, h, (width, height))
+    
+    return im1Reg, h
 
 def getROI(image, filename, noCropping=False, noMarkers=False):
     global clahe, marker_eroded_sub
